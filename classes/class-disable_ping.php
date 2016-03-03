094442ecb45c95e3254dc0b4d7f29d8c79cc3caa
Volkan Sah <VolkanSah@users.noreply.github.com>
2016-03-03 12:12:46 +0100

    Update and rename class-disable_ping to class-disable_ping.php

--- classes/class-disable_ping.php
+++ classes/class-disable_ping.php
@@ -5,5 +5,4 @@ function disable_self_trackback( &$links ) {
         if ( 0 === strpos( $link, get_option( 'home' ) ) )
             unset($links[$l]);
 }
-
 add_action( 'pre_ping', 'disable_self_trackback' );
